79c9cc536e193799d345dac2f49c79af996acc7d
anduuin <jcm2232@gmail.com>
2021-12-08 12:22:52 +0900

    Multithread

--- includes/constants.h
+++ includes/constants.h
@@ -3,19 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   constants.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cjeon <student.42seoul.kr>                 +#+  +:+       +#+        */
+/*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 10:52:57 by cjeon             #+#    #+#             */
-/*   Updated: 2021/12/07 13:57:51 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/12/08 00:40:11 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONSTANTS_H
 # define CONSTANTS_H
 
-# define MAX_ITER 50
+# define N_THREADS 16
+
+# define MAX_ITER 100
 
 # define WINDOW_W 1000
 # define WINDOW_H 1000
+
 
 #endif
--- srcs/paint_fractal.c
+++ srcs/paint_fractal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   paint_fractal.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cjeon <student.42seoul.kr>                 +#+  +:+       +#+        */
+/*   By: cjeon <cjeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 11:09:21 by cjeon             #+#    #+#             */
-/*   Updated: 2021/12/07 14:54:37 by cjeon            ###   ########.fr       */
+/*   Updated: 2021/12/08 00:36:02 by cjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,30 +38,56 @@ void	scale_by_view(t_complex *c, t_view *view)
 	c->i += view->im_start;
 }
 
+typedef struct s_paint_part_arg
+{
+	unsigned int *img;
+	int from;
+	int to;
+	t_fractal_func *ff;
+	t_view *view;
+	t_color *color;
+}	t_paint_part_arg;
+
+#include <unistd.h>
+#include <stdio.h>
+void	*paint_fractal_part(void *v_arg)
+{
+	t_paint_part_arg *arg;
+	t_complex c;
+	t_complex z;
+	int time;
+
+	arg = (t_paint_part_arg *)v_arg;
+	for (int i = arg->from; i < arg->to; i++) {
+		for (int j = 0; j < WINDOW_W; j++) {
+			c.i = i; c.r = j;
+			scale_by_window(&c);
+			scale_by_view(&c, arg->view);
+			arg->ff->init_complex(&z, &c);
+			time = arg->ff->calc_time(&z, &c);
+			arg->img[i * WINDOW_W + j] = get_color(arg->color, time);
+		}
+	}
+	return (NULL);
+}
+
 void	paint_fractal(unsigned int *img, t_fractal_func *ff, \
 						t_view *view, t_color *color)
 {
-	t_complex		c;
-	t_complex		z;
-	int				w;
-	int				h;
-	int				time;
+	pthread_t threads[N_THREADS];
+	int tasks;
+	int current_task;
+	void *ret;
 
-	h = 0;
-	while (h < WINDOW_H)
-	{
-		w = 0;
-		while (w < WINDOW_W)
-		{
-			c.r = w;
-			c.i = h;
-			scale_by_window(&c);
-			scale_by_view(&c, view);
-			ff->init_complex(&z, &c);
-			time = ff->calc_time(&z, &c);
-			*img++ = get_color(color, time);
-			w++;
-		}
-		h++;
+	tasks = WINDOW_H / (N_THREADS - 1);
+	current_task = 0;
+	for (int i = 0; i < N_THREADS - 1; i++) {
+		if (pthread_create(threads + i, NULL, paint_fractal_part, &(t_paint_part_arg){img, current_task, current_task + tasks, ff, view, color}) < 0)
+			exit(100);
+		current_task += tasks;
 	}
+	if (pthread_create(threads + N_THREADS - 1, NULL, paint_fractal_part, &(t_paint_part_arg){img, current_task, WINDOW_H, ff, view, color}) < 0)
+		exit(42);
+	for (int i = 0; i < N_THREADS; i++)
+		pthread_join(threads[i], &ret);
 }
